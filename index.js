5adeb15c8b5526f497cb66fdf970341d3068f45e
seshu babu karanam <108258542+seshubabukaranam@users.noreply.github.com>
2022-10-31 15:13:32 +0530

    docs - change in documentation

---
+++ index.js
@@ -0,0 +1,5 @@
+var inputDate = document.querySelector("#input-date");
+var btnShow = document.querySelector("#btn-show");
+var outputMessage = document.querySelector("#output-message");
+
+btnShow.addEventListener("click",clickHandler)
